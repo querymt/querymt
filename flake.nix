06609b1d2128840783c509ed5bcdd17b0d6cde31
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-02-05 18:35:25 +0000

    chore: nix shells fixed + migrated to flake parts

--- flake.nix
+++ flake.nix
@@ -1,50 +1,121 @@
 {
-  description = "Querymt development environment";
+  description = "QueryMT development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
     rust-overlay,
-    flake-utils,
+    flake-parts,
+    ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+      perSystem = {system, ...}: let
+        overlays = [(import rust-overlay)];
+        nixpkgsConfig = {
+          allowUnfree = true;
+        };
 
-      rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
-        extensions = ["rust-src"];
-      };
-    in {
-      # TODO: Add installation of qmt trough flake.
-      # Can't be done as for now due to: https://github.com/NixOS/nixpkgs/issues/359340
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config = nixpkgsConfig;
+        };
+
+        pkgsCuda =
+          if pkgs.stdenv.isLinux
+          then
+            import nixpkgs {
+              inherit system overlays;
+              config =
+                nixpkgsConfig
+                // {
+                  cudaSupport = true;
+                  cudaCapabilities = ["5.2" "6.0" "6.1" "7.0" "7.5" "8.0" "8.6"];
+                  cudaVersion = "12";
+                };
+            }
+          else null;
+
+        rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = ["rust-src"];
+        };
+
+        commonInputs = with pkgs; [
           rustToolchain
           openssl
           cmake
+          ninja
+          gnumake
           dbus
           clang
           llvmPackages.libclang
           rust-bindgen
           pkg-config
+          libgcc
+          gdb
         ];
+      in {
+        devShells =
+          {
+            default = pkgs.mkShell {
+              buildInputs = commonInputs;
 
-        shellHook =
-          /*
-          bash
-          */
-          ''
-            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
-            export PS1="(env:querymt) $PS1"
-          '';
+              shellHook =
+                /*
+                bash
+                */
+                ''
+                  export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+                  export PS1="(env:querymt) $PS1"
+                '';
+            };
+          }
+          // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+            vulkan = pkgs.mkShell {
+              buildInputs = commonInputs ++ (with pkgs; [vulkan-loader]);
+
+              shellHook =
+                /*
+                bash
+                */
+                ''
+                  export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+                  export LIBRARY_PATH="/run/opengl-driver/lib:${pkgs.lib.makeLibraryPath (with pkgs; [gcc.cc.lib vulkan-loader])}:$LIBRARY_PATH"
+                  export LD_LIBRARY_PATH="/run/opengl-driver/lib:${pkgs.lib.makeLibraryPath (with pkgs; [gcc.cc.lib vulkan-loader])}:$LD_LIBRARY_PATH"
+
+                  export PS1="(env:querymt-vulkan) $PS1"
+                '';
+            };
+
+            cuda = pkgsCuda.mkShell {
+              buildInputs =
+                commonInputs
+                ++ (with pkgsCuda; [
+                  cudaPackages.cudatoolkit
+                  cudaPackages.libcublas.static
+                ]);
+
+              shellHook =
+                /*
+                bash
+                */
+                ''
+                  export LIBCLANG_PATH="${pkgsCuda.llvmPackages.libclang.lib}/lib"
+                  export CUDA_PATH="${pkgsCuda.cudaPackages.cudatoolkit}"
+                  export LIBRARY_PATH="/run/opengl-driver/lib:${pkgsCuda.lib.makeLibraryPath (with pkgsCuda; [gcc.cc.lib cudaPackages.cudatoolkit])}:$LIBRARY_PATH"
+                  export LD_LIBRARY_PATH="/run/opengl-driver/lib:${pkgsCuda.lib.makeLibraryPath (with pkgsCuda; [gcc.cc.lib cudaPackages.cudatoolkit])}:$LD_LIBRARY_PATH"
+                  export RUSTFLAGS="-L native=$CUDA_PATH/lib -L native=${pkgsCuda.cudaPackages.libcublas.static}/lib"
+
+                  export PS1="(env:querymt-cuda) $PS1"
+                '';
+            };
+          };
       };
-    });
+    };
 }
